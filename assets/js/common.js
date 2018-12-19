85a24c52dc0a1d395d2d9fe86a498a5390f4a89c
alex <sv5060@gmail.com>
2018-12-19 14:09:10 +0900

    default layout

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,16 +2,17 @@ if (!String.prototype.format) {
     String.prototype.format = function () {
         var args = arguments;
         return this.replace(/{(\d+)}/g, function (match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
+            return typeof args[number] != 'undefined' ? args[number] : match;
         });
     };
 }
 
-$(function(){
-    $('.menu-icon').click(function(){
-       console.log(33)
+$(function () {
+    $('.menu__toggle__icon').click(function () {
+        if ($('body').hasClass('menu--open')) {
+            $('body').removeClass('menu--open');
+        } else {
+            $('body').addClass('menu--open');
+        }
     });
 })

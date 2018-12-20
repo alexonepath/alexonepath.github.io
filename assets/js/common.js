f3b38eb17c59adeac58cb18bc89d271ad3b8c4bb
alex <sv5060@gmail.com>
2018-12-20 15:17:37 +0900

    default layout

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,6 @@
-require('../css/font.css')
-require('../css/svg.css')
-require('../css/accordion.css')
-require('../css/layout.css')
-
 if (!String.prototype.format) {
     String.prototype.format = function () {
-        var args = arguments;
+        let args = arguments;
         return this.replace(/{(\d+)}/g, function (match, number) {
             return typeof args[number] != 'undefined' ? args[number] : match;
         });
